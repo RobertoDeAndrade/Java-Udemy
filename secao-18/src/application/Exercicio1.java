9ae6fbf75b31cec9e9c1ebf57e19d5f8a1e17ab8
Roberto <roberto_canto@hotmail.com>
2023-03-03 22:00:24 -0300

    Exercício de fixação do módulo

---
+++ secao-18/src/application/Exercicio1.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Contract;
+import model.entities.Installment;
+import model.services.ContratcService;
+import model.services.PaypalService;
+
+public class Exercicio1 {
+
+	public static void main(String[] args) throws ParseException {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		System.out.println("Entre com os dados do contrato:");
+		System.out.print("Número: ");
+		int number = sc.nextInt();
+		System.out.print("Data (dd/MM/yyy): ");
+		LocalDate date = LocalDate.parse(sc.next(), fmt);
+		System.out.print("Valor do contrato: ");
+		double totalValue = sc.nextDouble();
+		
+		Contract contract = new Contract(number, date, totalValue);
+		
+		System.out.print("Entre com o número de parcelas: ");
+		int n = sc.nextInt();
+		
+		ContratcService service = new ContratcService(new PaypalService());
+		service.processContract(contract, n);
+		
+		System.out.println("Parcelas: ");
+		for (Installment installment :  contract.getInstallments()) {
+			System.out.println(installment);
+		}
+		
+		sc.close();
+
+	}
+
+}
