728ec93a25747157726457d71c4cc7c5c12b044a
Roberto <roberto_canto@hotmail.com>
2023-03-07 22:29:08 -0300

    Set

---
+++ secao-19/src/application/Exemplo7.java
@@ -0,0 +1,21 @@
+package application;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Exemplo7 {
+	public static void main(String[] args) {
+
+		Set<String> set = new LinkedHashSet<>();
+
+		set.add("Tv");
+		set.add("Tablet");
+		set.add("Notebook");
+
+		set.removeIf(x -> x.charAt(0) == 'T');
+
+		for (String p : set) {
+			System.out.println(p);
+		}
+	}
+}
