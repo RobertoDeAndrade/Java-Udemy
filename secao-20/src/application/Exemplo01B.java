9a7675fdae405f726822f59965c061fc2613fcba
Roberto <roberto_canto@hotmail.com>
2023-03-08 21:34:12 -0300

    Predicate

--- secao-20/src/application/Exemplo01B.java
+++ secao-20/src/application/Exemplo01B.java
@@ -7,7 +7,7 @@ import java.util.Locale;
 
 import entities.Product;
 
-public class Exemplo02 {
+public class Exemplo01B {
 
 	public static void main(String[] args) {
 
--- secao-20/src/application/Exemplo02E.java
+++ secao-20/src/application/Exemplo02E.java
@@ -3,28 +3,29 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Predicate;
 
-import entities.MyComparator;
 import entities.Product;
 
-public class Exemplo01 {
+public class Exemplo02E {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 
 		List<Product> list = new ArrayList<>();
 
-		list.add(new Product("TV", 900.00));
-		list.add(new Product("Notebook", 1200.00));
-		list.add(new Product("Tablet", 450.00));
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.50));
+		list.add(new Product("HD Case", 80.90));
 
-		list.sort(new MyComparator());
+		double min = 100.0;
+
+		list.removeIf(p -> p.getPrice() >= min);
 
 		for (Product p : list) {
 			System.out.println(p);
 		}
-
 	}
-
 }
