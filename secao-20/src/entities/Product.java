4654f0c1882b4c2f1d41859044b7e7abba8e7eaf
Roberto <roberto_canto@hotmail.com>
2023-03-08 22:19:14 -0300

    Function

--- secao-20/src/entities/Product.java
+++ secao-20/src/entities/Product.java
@@ -26,20 +26,34 @@ public class Product {
 		this.price = price;
 	}
 
+	// Example 2
 	public static boolean staticProductPredicate(Product p) {
 		return p.getPrice() >= 100.0;
 	}
 
+	// Example 2
 	public boolean nonStaticProductPredicate() {
 		return price >= 100.0;
 	}
 
+	// Example 3
 	public static void staticPriceUpdate(Product p) {
 		p.setPrice(p.getPrice() * 1.1);
 	}
 
+	// Example 3
 	public void nonStaticPriceUpdate() {
 		price += price * 0.1;
+	}
+	
+	// Example 4
+	public static String staticUpperCaseName(Product p) {
+		return p.getName().toUpperCase();
+	}
+
+	// Example 4
+	public String nonStaticUpperCaseName() {
+		return name.toUpperCase();
 	}
 
 	@Override
